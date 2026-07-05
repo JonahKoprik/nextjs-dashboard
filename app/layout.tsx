b454eaf7781274dec7d1a3f739abab18ce926b97
linkotech <johnzdj18@gmail.com>
2026-07-05 12:59:05 +1000

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore: CSS side-effect import declaration missing
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className= {'${inter.className} antialiased'}>{children}</body>
+    </html>
+  );
+}
